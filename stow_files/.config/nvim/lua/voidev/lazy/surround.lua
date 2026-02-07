da40781f89543da8546d2861dec14be559e43019
v0juse <vitor__d@hotmail.com>
2026-02-07 11:03:29 -0300

    feat: vim surround

---
+++ stow_files/.config/nvim/lua/voidev/lazy/surround.lua
@@ -0,0 +1,10 @@
+return {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- No configuration needed for default behavior
+        })
+    end,
+}
