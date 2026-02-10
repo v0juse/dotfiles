97b2321064958a1f8fc9ec0c23eaebd003a6cef2
v0juse <vitor__d@hotmail.com>
2026-02-09 23:00:14 -0300

    chore: mappings for telescope

--- stow_files/.config/nvim/lua/voidev/lazy/telescope.lua
+++ stow_files/.config/nvim/lua/voidev/lazy/telescope.lua
@@ -71,6 +71,10 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+        vim.keymap.set('v', '<leader>sg', function()
+            local selection = vim.fn.getvisual()
+            builtin.grep_string({ search = selection })
+        end, { desc = '[S]earch [V]isual Selection' })
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
